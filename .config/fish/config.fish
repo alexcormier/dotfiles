a5025057e2b5b3aff43e814691573cb60f8d07fb
Alexandre Cormier <alexandre@cormier.io>
2020-09-10 21:09:16 -0400

    fix fish config load order

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,17 +5,17 @@ if set -q XDG_CONFIG_HOME
     set FISH_CONFIG_HOME {$XDG_CONFIG_HOME}/fish
 end
 
-set -l login_file {$FISH_CONFIG_HOME}/login.fish
 set -l env_file {$FISH_CONFIG_HOME}/env.fish
+set -l login_file {$FISH_CONFIG_HOME}/login.fish
 set -l interactive_file {$FISH_CONFIG_HOME}/interactive.fish
 set -l logout_file {$FISH_CONFIG_HOME}/logout.fish
 
-if status is-login; and test -f {$login_file}
-    source {$login_file}
-end
-
 if test -f {$env_file}
     source {$env_file}
+end
+
+if status is-login; and test -f {$login_file}
+    source {$login_file}
 end
 
 if status is-interactive; and test -f {$interactive_file}
